d6f1eefac8969f3a5923fc66248dc2bafe30ef85
Harout Mardirossian <harmardir@users.noreply.github.com>
2023-10-17 16:09:37 +0300

    Creating function based registration view

--- register/views.py
+++ register/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import RegForm
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("View: registration index")
 
-
+def reg_view(request):
+    context = {}
+    context['form'] = RegForm()
+    return render(request, "reg.html", context)
 
